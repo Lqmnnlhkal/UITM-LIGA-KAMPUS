682babb26a1d68b76cbc5c1f5bdac73eb302ea5a
Lqmnnlhkal <155933569+Lqmnnlhkal@users.noreply.github.com>
2026-06-07 22:02:32 +0800

    Add files via upload
    Assalamualaikum Sir, inside here is the HTML files for our project Liga Kampus including the ERD.

---
+++ Storyboard/STORYBOARD/public_html/js/script.js
@@ -0,0 +1,7 @@
+function mockSave(message) {
+    alert(message + "\n\nThis is a storyboard prototype. No database has been linked or added!");
+}
+
+function mockDelete(item) {
+    confirm("Storyboard mockup: Delete " + item + "?");
+}
